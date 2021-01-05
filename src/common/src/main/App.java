4cc26b6d01b5ecd8c5f47e067aa88d05dc901670
SirDancealot <mj@lpj-software.dk>
2021-01-05 16:35:14 +0100

    Created minimal host

--- src/common/src/main/App.java
+++ src/common/src/main/App.java
@@ -8,18 +8,18 @@ import java.io.IOException;
 public class App {
 
 	public static void main(String[] argv) throws InterruptedException, IOException {
-		/*
 		Space inbox = new SequentialSpace();
 		SpaceRepository inboxR = new SpaceRepository();
 		inboxR.add("inbox", inbox);
-		inboxR.addGate("tcp://localhost:31415/?keep");
+		inboxR.addGate("tcp://192.168.1.62:33333/?keep");
 
 		Object[] tup = inboxR.get("inbox").get(new FormalField(String.class));
 		System.out.println(tup[0]);
-		*/
 
+		/*
 		RemoteSpace outbox = new RemoteSpace("tcp://188.178.206.128:33333/inbox?keep");
 		outbox.put("goodbye");
+		 */
 
 	}
 
