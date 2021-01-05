4e6dc04c94eb2a4cb520f4afd43cddc6bfbb8716
SirDancealot <mj@lpj-software.dk>
2021-01-05 16:03:34 +0100

    initial commit

---
+++ src/common/src/main/App.java
@@ -0,0 +1,26 @@
+package common.src.main;
+
+import org.jspace.*;
+
+import java.io.IOException;
+
+
+public class App {
+
+	public static void main(String[] argv) throws InterruptedException, IOException {
+		/*
+		Space inbox = new SequentialSpace();
+		SpaceRepository inboxR = new SpaceRepository();
+		inboxR.add("inbox", inbox);
+		inboxR.addGate("tcp://localhost:31415/?keep");
+
+		Object[] tup = inboxR.get("inbox").get(new FormalField(String.class));
+		System.out.println(tup[0]);
+		*/
+
+		RemoteSpace outbox = new RemoteSpace("tcp://188.178.206.128:33333/inbox?keep");
+		outbox.put("goodbye");
+
+	}
+
+}
