758df5c11cd819c15f7410c07b34b53d92777b56
Fipzz <43668537+Fipzz@users.noreply.github.com>
2021-01-12 15:18:31 +0100

    Work done on both UI for setup and game.

--- src/common/src/UI/SetupGUI.java
+++ src/common/src/UI/SetupGUI.java
@@ -3,13 +3,12 @@ package common.src.UI;
 import common.src.main.App;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SetupGUI extends JFrame implements ActionListener {
 
-    private JPanel mainPanel;
+    private JPanel setupPanel;
     private JTextField LocalPort;
     private JTextField HostIP;
     private JTextField HostPort;
@@ -18,7 +17,7 @@ public class SetupGUI extends JFrame implements ActionListener {
 
     public SetupGUI (){
 
-        this.setContentPane(mainPanel);
+        this.setContentPane(setupPanel);
         this.pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Launch settings");
@@ -38,15 +37,25 @@ public class SetupGUI extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e){
         if(e.getSource() == connectButton){
 
-            new Thread(new App(hostCheckBox.isSelected(),HostIP.getText(),HostPort.getText(),LocalPort.getText())).start();
+            //Start backend
+            //new Thread(new App(hostCheckBox.isSelected(),HostIP.getText(),HostPort.getText(),LocalPort.getText())).start();
 
-            //TODO Launch GUI with info from backend
+            //Launch gameGUI
+            JFrame gameGUI = new GameGUI();
+            gameGUI.setVisible(true);
+
+            dispose();
 
         } else if (e.getSource() == hostCheckBox){
-            if(hostCheckBox.isSelected())
+            if(hostCheckBox.isSelected()){
+
                 connectButton.setText("Host");
-            else
+                HostIP.setEditable(false);
+            } else {
+
                 connectButton.setText("Connect");
+                HostIP.setEditable(true);
+            }
         }
     }
 }
