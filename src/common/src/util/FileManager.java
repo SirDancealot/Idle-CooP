b6795f2511a01853d855601d481c49c50589e7c2
SirDancealot <mj@lpj-software.dk>
2021-01-16 16:49:04 +0100

    Added a FileManager to load and save files

---
+++ src/common/src/util/FileManager.java
@@ -0,0 +1,35 @@
+package common.src.util;
+
+import java.io.*;
+
+public class FileManager {
+	public static <E> E loadObject(String path) {
+		try {
+			File f = new File(path);
+			E obj;
+			if (!f.exists())
+				return null;
+			FileInputStream fis = new FileInputStream(path);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			obj = (E)ois.readObject();
+			ois.close();
+			fis.close();
+			return obj;
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static <E> void saveObject(String path, E obj) {
+		try {
+			FileOutputStream fos = new FileOutputStream(path);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(obj);
+			oos.close();
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
